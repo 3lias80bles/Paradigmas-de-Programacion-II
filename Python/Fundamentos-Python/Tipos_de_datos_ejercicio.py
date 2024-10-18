f9d047eec24306859e10f0a7f073a6e9bd1720aa
Sergio-Elias <roblesergioelias@gmail.com>
2024-10-18 11:24:02 -0600

    Se actualizan los trabajos de Tipo de datos

--- Python/Fundamentos-Python/Tipos_de_datos_ejercicio.py
+++ Python/Fundamentos-Python/Tipos_de_datos_ejercicio.py
@@ -1,32 +1,38 @@
+'''
+Sergio Elias Robles Ignacio
+13 de octubre de 2024.
+Descripción:
+En este ejercicio se usan los tipos basicos de datos en un pequeño programa
+'''
 print("*** Gastos diarios ***")
-variable_texto = "Lunes"  # Asigna el valor "Lunes" a la variable "variable_texto" e imprime
+variable_texto = "Lunes"  # Asigna el valor de cadena a la variable "variable_texto" e imprime
 print("Día: ", variable_texto)
 
-pasaje = 20  # Asigna el valor 20 a la variable "pasaje" e imprime
+pasaje = 20  # Asigna el valor entero a la variable "pasaje" e imprime
 print("Pasaje: ", pasaje)
 
-comida = 135.5  # Asigna el valor 135.5 a la variable "comida" e imprime
+comida = 135.5  # Asigna el valor decimal a la variable "comida" e imprime
 print("Comida: ", comida)
 
-otros_gastos = 34.5  # Asigna el valor 34.5 a la variable "otros_gastos" e imprime
+otros_gastos = 34.5  # Asigna el valor decimal a la variable "otros_gastos" e imprime
 print("Otros gastos: ", otros_gastos)
 
-variable_booleana = True  # Asigna el valor booleano True a la variable "variable_booleana" e imprime
+variable_booleana = True  # Asigna el valor booleano a la variable "variable_booleana" e imprime
 print("¿Fue mayor a mi presupuesto?: ", variable_booleana)
 print()
 
 print("*** Gastos diarios ***")
-variable_texto = "Martes"  # Asigna el valor "Martes" a la "variable variable_texto" e imprime
+variable_texto = "Martes"  # Asigna el valor de cadena a la variable "variable_texto" e imprime
 print("Día: ", variable_texto)
 
-pasaje = 12  # Asigna el valor 12 a la variable "pasaje" e imprime
+pasaje = 12  # Asigna el valor entero a la variable "pasaje" e imprime
 print("Pasaje: ", pasaje)
 
-comida = 45.5  # Asigna el valor 45.5 a la variable "comida" e imprime
+comida = 45.5  # Asigna el valor decimal a la variable "comida" e imprime
 print("Comida: ", comida)
 
-otros_gastos = 4.5  # Asigna el valor 4.5 a la variable "otros_gastos" e imprime
+otros_gastos = 4.5  # Asigna el valor decimal a la variable "otros_gastos" e imprime
 print("Otros gastos: ", otros_gastos)
 
-variable_booleana = False  # Asigna el valor booleano False a la variable "variable_booleana" e imprime
+variable_booleana = False  # Asigna el valor booleano a la variable "variable_booleana" e imprime
 print("¿Fue mayor a mi presupuesto?: ", variable_booleana)
