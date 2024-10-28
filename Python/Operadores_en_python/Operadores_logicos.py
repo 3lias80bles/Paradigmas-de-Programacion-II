15239635e56ceedcff9255d1cf07ad81287947b3
Sergio-Elias <roblesergioelias@gmail.com>
2024-10-28 16:48:13 -0600

    Se suben ejercicios de Operadores_logicos.py y Operadores_relacionales.py

--- Python/Operadores_en_python/Operadores_logicos.py
+++ Python/Operadores_en_python/Operadores_logicos.py
@@ -1,23 +1,32 @@
 '''
 Nombre: Sergio Elias Robles Ignacio
 Fecha:18 de Octubre de 2024
-Descripción:
+Descripción: En este trabajo se realizan operaciones lógicas básicas con los valores ingresados.
 '''
 
-si_no=input("Ingresa Si o No: ")
-si_no = si_no.lower() == "si"
-print(f"El resultado en boleano es: {si_no}")
+print("*** Operaciones logicas ***")
+#Se pide un dato de tipo String al usuario
+si_no = input("Ingresa Si o No: ")
+si_no = si_no.lower() == "si"  #Convierte la cadena a minúsculas y compara si es igual a "si"
+print(f"El resultado en booleano es: {si_no}")  #Muestra si el resultado es True o False
+
 print()
-no_si=input("Ingresa Si o No: ")
+# Se repite el proceso anterior
+no_si = input("Ingresa Si o No: ")
 no_si = no_si.lower() == "si"
+print(f"El resultado en booleano es: {no_si}")  #Muestra si el resultado es True o False
+
+print()  # Salto de línea
+
+#Se usan los operadores lógicos básicos: "and", "or" y "not".
+print(f"¿Los dos valores que ingresaste son iguales? {si_no and no_si}")  # "and" verifica si ambos valores son verdaderos
+
 print()
-print(f"El resultado en boleano es: {si_no}")
+print(f"¿Al menos uno de los valores que ingresaste es verdadero? {si_no or no_si}")  # "or" verifica si al menos uno es verdadero
+
 print()
-print(f"Los dos valores que ingresaste son iguales? {si_no and no_si}")
+print(f"La negación del primer valor es: {not si_no}")  # "not" invierte el valor booleano del primer valor
+
 print()
-print(f"Los dos valores que ingresaste son diferentes? {si_no or no_si}")
-print()
-print(f"La negación del primero valor es: {not si_no}")
-print()
-print(f"La negación del segundo valor es: {not no_si}")
+print(f"La negación del segundo valor es: {not no_si}")  # "not" invierte el valor booleano del segundo valor
 
--- Python/Operadores_en_python/Operadores_relacionales.py
+++ Python/Operadores_en_python/Operadores_relacionales.py
@@ -1,28 +1,37 @@
 '''
 Nombre: Sergio Elias Robles Ignacio
 Fecha:18 de Octubre de 2024
-Descripción:
+Descripción: En este progrma se realizan comparaciones basicas con los operadores relacionales.
 '''
 
-num1=float(input("ingrese un número: "))
-num2=float(input("ingrese el segundo número: "))
+# Solicita al usuario ingresar dos números  en este caso son de tipo flotante decimal
+num1 = float(input("Ingrese un número: "))
+num2 = float(input("Ingrese el segundo número: "))
 
-mayor=num1>num2
+#Se usa la función de {num1:.2f} para todos los casos esto para saber cuantos decimales tendra el número.
+#Se compara si el primer número es mayor que el segundo.
+mayor = num1 > num2
 print(f"¿El número {num1:.2f} es mayor que {num2:.2f}?: {mayor}")
 
-mayor_igual=num1>=num2
-print(f"¿El número {num1:.2f} es mayor igual que {num2:.2f}?: {mayor_igual}")
+#Se compara si el primer número es mayor o igual al segundo.
+mayor_igual = num1 >= num2
+print(f"¿El número {num1:.2f} es mayor o igual que {num2:.2f}?: {mayor_igual}")
 
-menor=num1<num2
+#Se compara si el primer número es menor que el segundo.
+menor = num1 < num2
 print(f"¿El número {num1:.2f} es menor que {num2:.2f}?: {menor}")
 
-menor_igual=num1<=num2
-print(f"¿El número {num1:.2f} es menor igual que {num2}?: {mayor_igual}")
+#Se compara si el primer número es menor o igual al segundo.
+menor_igual = num1 <= num2
+print(f"¿El número {num1:.2f} es menor o igual que {num2:.2f}?: {menor_igual}")
 
-igual=num1==num2
+#Se compara si el primer número es igual al segundo.
+igual = num1 == num2
 print(f"¿El número {num1:.2f} es igual que {num2:.2f}?: {igual}")
 
-diferente=num1!=num2
+#Se compara si el primer número es diferente al segundo.
+diferente = num1 != num2
 print(f"¿El número {num1:.2f} es diferente que {num2:.2f}?: {diferente}")
 
-print(f"¿El número {num1:.2f} es diferente  que {num2:.2f}?: {num1!=num2}")
+# Otra manera de imprimir la comparación de diferencia es ponerla directamente en el print.
+print(f"¿El número {num1:.2f} es diferente que {num2:.2f}?: {num1 != num2}")
