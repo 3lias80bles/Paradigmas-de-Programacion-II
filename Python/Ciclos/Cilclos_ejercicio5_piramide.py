3e804bf35a9d4d6415d09a91ff8160686defefb3
Sergio-Elias <roblesergioelias@gmail.com>
2024-12-02 19:58:56 -0600

    Se sube actualización de Ciclos_ejercicio5_piramide

--- Python/Ciclos/Cilclos_ejercicio5_piramide.py
+++ Python/Ciclos/Cilclos_ejercicio5_piramide.py
@@ -4,41 +4,56 @@ Fecha: 29 de Octubre de 2024
 Descripción: En este programa se utiliza el ciclo for
 '''
 
+#Se definene las variables de espacios y asteriscos
 espacio=" "
 asterisco= "*"
-n=4
 i=1
 
+#Se pide el tamaño del la piramide con un numero desde el teclado
+tama = int(input("Ingresa el número de filas de la piramide: "))
 print()
-print("Piramide 1")
-for i in range (1,n+1):
-    espacios = espacio * (n - i)
-    asteriscos = asterisco * i
-    print(f"{asteriscos}{espacios}")
+
+# Pirámide 1: Crece hacia la izquierda
+print("*** Piramide 1 ***")
+for i in range (1,tama+1): #Itera desde 1 hasta el número de filas
+    espacios = espacio * (tama - i) #Decrementa los espacios en cada fila empezando del tamaño menos 1
+    asteriscos = asterisco * i #Incrementa los asteriscos en cada fila
+    print(f"{asteriscos}{espacios}") #Imprime los asteriscos seguidos de espacios
 print()
-print("piramide 2")
-for i in range (1,n+1):
-    asteriscos = asterisco * ((n-i)+1)
-    espacios = espacio * i
-    print(f"{asteriscos}{espacios}")
+
+# Pirámide 1: Crece hacia la izquierda
+print("*** Piramide 2 ***")
 print()
-print("Piramide 3")
-for i in range(1, n + 1):
-    espacios = espacio * (n - i)
-    asteriscos = asterisco * (2 * i - 1)
-    print(f"{espacios}{asteriscos}")
+for i in range (1,tama+1): #Itera desde 1 hasta el número de filas
+    asteriscos = asterisco * ((tama-i)+1) #Decrementa los asteriscos
+    espacios = espacio * i #aumenta los espacios
+    print(f"{asteriscos}{espacios}")#Imprime los asteriscos seguidos de espacios
+print()
+
+# Pirámide 1: Crece hacia la izquierda
+print("*** Piramide 3 ***") #Itera desde 1 hasta el número de filas.
+print()
+for i in range(1, tama + 1): #Itera desde 1 hasta el número de filas.
+    espacios = espacio * (tama - i) #Calcula los espacios para centrar la pirámide.
+    asteriscos = asterisco * (2 * i - 1) #Incrementa los asteriscos de forma impar.
+    print(f"{espacios}{asteriscos}")#Imprime los espacios seguidos de asteriscos.
 
 print()
-print("Piramide 4")
-for i in range (1,n+1):
-    espacios = espacio * (n - i)
-    asteriscos = asterisco * i
-    print(f"{espacios}{asteriscos}")
 
+# Pirámide 1: Crece hacia la izquierda
+print("*** Piramide 4 ***")#Itera desde 1 hasta el número de filas
 print()
-print("piramide Extra")
-for i in range (1,n+1):
-    asteriscos = asterisco * ((n-i)+1)
+for i in range (1,tama+1): #Itera desde 1 hasta el número de filas.
+    espacios = espacio * (tama - i) #Calcula los espacios para centrar la pirámide.
+    asteriscos = asterisco * i #Aumenta los asteriscos
+    print(f"{espacios}{asteriscos}") #Imprime los espacios seguidos de asteriscos.
+print()
+
+# Pirámide 1: Crece hacia la izquierda
+print("*** Piramide Extra ***")#Itera desde 1 hasta el número de filas
+print()
+for i in range (1,tama+1):
+    asteriscos = asterisco * ((tama-i)+1)
     espacios = espacio * (i-1)
     print(f"{espacios}{asteriscos}")
 
