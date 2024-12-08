2018c720b95cb33baf20a7f62c317126dd3d33fe
Sergio-Elias <roblesergioelias@gmail.com>
2024-12-08 17:59:11 -0600

    Se sube correccion de Listas_ej1_playlist.py

--- Python/Otros-temas/Listas_ej1_playlist.py
+++ Python/Otros-temas/Listas_ej1_playlist.py
@@ -1,7 +1,7 @@
 '''
 Nombre: Sergio Elias Robles Ignacio
 Fecha: 25 de Octubre de 2024
-Descripción: En este trabajo se usan listas para realizar una playlist de videos.
+Descripción: En este trabajo se usan listas y funciones para realizar una playlist de videos.
 '''
 
 #Inicialización de variables.
@@ -9,8 +9,7 @@ i=0
 opcion=7
 playlist = []
 
-#Bucle para el menú principal de la calculadora.
-while opcion!=0:
+def menu ():
     print()
     print("*** Playlist de videos de YouTube ***")
     print("Ver playlist por videos añadidos...................[1]")
@@ -21,6 +20,69 @@ while opcion!=0:
     print("Eliminar video de la playlist......................[6]")
     print("Salir..............................................[0]")
     print()
+
+
+#funciones de operaciones.
+def por_ingreso():
+    if len(playlist):
+        print("*** Playlist actual ***")
+        for i in range(len(playlist)):
+            print(f"[{i + 1}]....{playlist[i]}")
+    else:
+        print("No hay videos para mostrar.")
+
+def acendente():
+    playlist.sort()
+    if len(playlist):
+        print("*** Playlist actual por orden ascendente (A-Z) ***")
+        for i in range(len(playlist)):
+            print(f"[{i + 1}]....{playlist[i]}")
+    else:
+        print("No hay videos para mostrar.")
+    print()
+
+def decendente():
+    print("*** Playlist actual por orden descendente (Z-A) ***")
+    if len(playlist):
+        playlist.sort(reverse=True)
+        for i in range(len(playlist)):
+            print(f"[{i + 1}]....{playlist[i]}")
+    else:
+        print("No hay videos para mostrar.")
+    print()
+
+def agregar():
+    nuevo = input("Ingrese el nombre del video a agregar a la playlist: ")
+    playlist.append(nuevo)
+    print("Video agregado exitosamente.")
+    print()
+
+def agregar_muchos():
+    cantidad = int(input("Ingrese la cantidad de videos a agregar: "))
+    for i in range(cantidad):
+        nuevo = input(f"Ingrese el nombre del video {i + 1}: ")
+        playlist.append(nuevo)
+    print("Videos agregados exitosamente.")
+    print()
+
+def eliminar():
+    if len(playlist):
+        print("*** Playlist actual ***")
+        for i in range(len(playlist)):
+            print(f"[{i + 1}]....{playlist[i]}")
+        elim = int(input("Ingrese el indice del video a eliminar: "))
+        if 1 <= elim <= len(playlist):
+            eliminado = playlist.pop(elim - 1)
+            print(f"El video '{eliminado}' ha sido eliminado.")
+        else:
+            print("Favor de ingresar un indice de video valido.")
+    else:
+        print("No hay videos para eliminar.")
+
+
+#Bucle para el menú principal de la calculadora.
+while opcion!=0:
+    menu()
     #Se solicitar una opción al usuario.
     opcion=int(input("Su opción es: "))
 
@@ -29,65 +91,27 @@ while opcion!=0:
         print("Adios...")
     #Ver playlist por videos añadidos
     elif opcion == 1:
-        if len(playlist):
-            print("*** Playlist actual ***")
-            for i in range(len(playlist)):
-                print(f"[{i+1}]....{playlist[i]}")
-        else:
-            print("No hay videos para mostrar.")
+        por_ingreso()
 
     #Ver playlist por orden ascendente (A-Z)
     elif opcion == 2:
-        playlist.sort()
-        if len(playlist):
-            print("*** Playlist actual por orden ascendente (A-Z) ***")
-            for i in range(len(playlist)):
-                print(f"[{i+1}]....{playlist[i]}")
-        else:
-            print("No hay videos para mostrar.")
-        print()
+        acendente()
 
     #Ver playlist por orden descendente (Z-A)
     elif opcion == 3:
-        print("*** Playlist actual por orden descendente (Z-A) ***")
-        if len(playlist):
-            playlist.sort(reverse=True)
-            for i in range(len(playlist)):
-                print(f"[{i+1}]....{playlist[i]}")
-        else:
-            print("No hay videos para mostrar.")
-        print()
+        decendente()
 
     #Añadir video de YouTube a la playlist.
     elif opcion == 4:
-        nuevo = input("Ingrese el nombre del video a agregar a la playlist: ")
-        playlist.append(nuevo)
-        print("Video agregado exitosamente.")
-        print()
+        agregar()
 
     #Añadir varios videos de YouTube a la playlist.
     elif opcion == 5:
-        cantidad = int(input("Ingrese la cantidad de videos a agregar: "))
-        for i in range(cantidad):
-            nuevo = input(f"Ingrese el nombre del video {i + 1}: ")
-            playlist.append(nuevo)
-        print("Videos agregados exitosamente.")
-        print()
+        agregar_muchos()
 
     #Eliminar video de la playlist.
     elif opcion == 6:
-        if len(playlist):
-            print("*** Playlist actual ***")
-            for i in range(len(playlist)):
-                print(f"[{i+1}]....{playlist[i]}")
-            elim = int(input("Ingrese el indice del video a eliminar: "))
-            if 1 <= elim <= len(playlist):
-                eliminado = playlist.pop(elim - 1)
-                print(f"El video '{eliminado}' ha sido eliminado.")
-            else:
-                print("Favor de ingresar un indice de video valido.")
-        else:
-            print("No hay videos para eliminar.")
+        eliminar()
 
     # Opciones no válidas.
     else:
