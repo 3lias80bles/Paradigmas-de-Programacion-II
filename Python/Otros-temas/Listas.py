6a47ef7ac1eed6a75642b200bbaba37f10f178ee
Sergio-Elias <roblesergioelias@gmail.com>
2024-12-08 17:37:58 -0600

    Se sube correccion de Listas.py y Listas_ej1_playlist.py

--- Python/Otros-temas/Listas.py
+++ Python/Otros-temas/Listas.py
@@ -8,33 +8,38 @@ Descripción: En este programa se utiliza el ciclo for
 #[0]      [1]   [2]   [3]    [4]    [5]
 #Amelia albert Kevin Elton Magdiel Diana
 
-alumnos = ["Amelia","Albert"]
+#Lista de alumnos inicial
+alumnos = ["Amelia", "Albert"]
 
 print(f"Alumnos de la lista: {alumnos}")
 
-alumnos.append("Kevin")#Añade un elemento al dinal de la lista
+alumnos.append("Kevin")  #Se añade un elemento al final de la lista
+alumnos.append("Diana")
 
-alumnos.append("Diana")#Añade un elemento en un índice especifico
-alumnos.insert(3,"Elton")
+#Añade un elemento en un índice especifico
+alumnos.insert(3, "Elton")
+alumnos.insert(4, "Magdiel")
 
-alumnos.insert(4,"Magdiel")
-
+# Añadiendo más elementos al final de la lista
 alumnos.append("Eden")
 alumnos.append("Sergio")
 
-alumnos.insert(7,"Magdiel")
+# Inserta nuevamente a 'Magdiel' en la posición 7
+alumnos.insert(7, "Magdiel")
 alumnos.append("Magdiel")
 
 #Borra un elemento por su valor
 alumnos.remove("Magdiel")
 
-#Borrar por su índice
-alumnos.pop(6)
-#otra manera
-del alumnos [7]
+#Borrar un elemento por su índice
+alumnos.pop(6)  # Elimina el elemento en el índice 6
+del alumnos[7]  # Otra manera de eliminar un elemento por su índice
 
-alumnos.len()
+#Verifica la longitud de la lista
+len(alumnos)
+
+# Ordena la lista alfabéticamente
 alumnos.sort()
-alumnos.sort(reserve=true)
+alumnos.sort(reverse=True)
 
-print(f"{alumnos}")
+print(f"Alumnos después de las modificaciones: {alumnos}")
